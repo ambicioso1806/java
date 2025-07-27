3eb1bb2a31562053cde3b0972a3d4609266d69de
ambicioso1806 <ramji06singh@gmail.com>
2025-07-27 23:47:00 +0530

    Create floorandceil.java

---
+++ java_basics/floorandceil.java
@@ -0,0 +1,14 @@
+
+// floor and ceil
+
+public class floorceil {
+    public static void main(String[] args) {
+        int a =6,b=3;
+        double r = b/a;
+        double s =Math.floor(r);
+        double t =Math.ceil(r);
+        System.out.println(s);
+        System.out.println(t);
+
+    }
+}

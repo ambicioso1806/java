6c4035463ff201ac7081ed10fb45ca5c2c83cefc
ambicioso1806 <ramji06singh@gmail.com>
2025-07-29 23:44:07 +0530

    Create pattern1.java

---
+++ java_basics/pattern1.java
@@ -0,0 +1,16 @@
+
+// right hand trianle pattern
+
+public class pattern1 {
+    public static void main(String[] args) {
+        int i =0,j=0,row=5;
+        for( i=0;i<5;i++)
+        {
+            for( j =0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

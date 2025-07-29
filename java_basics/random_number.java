063ccc8dae31b513039232aae5f48c8bcaf46daa
ambicioso1806 <ramji06singh@gmail.com>
2025-07-29 23:27:37 +0530

    Create random_number.java

---
+++ java_basics/random_number.java
@@ -0,0 +1,10 @@
+
+// RANDOM NUMBER
+
+import java.util.*;
+public class randomnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1st random number." + Math.random());
+    }
+}

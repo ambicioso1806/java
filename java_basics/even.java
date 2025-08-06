d60522591dfebb6cd31b8e77c091340eb433629a
ambicioso1806 <ramji06singh@gmail.com>
2025-08-06 21:47:49 +0530

    Create even.java

---
+++ java_basics/even.java
@@ -0,0 +1,12 @@
+
+//Java program to display even numbers from 1 to 100
+
+public class even {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

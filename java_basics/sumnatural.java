2e948e93a938765d8335a22b27dc46506a6a0cb1
ambicioso1806 <ramji06singh@gmail.com>
2025-08-07 21:05:11 +0530

    Create sumnatural.java

---
+++ java_basics/sumnatural.java
@@ -0,0 +1,13 @@
+
+//Find Sum of Natural Numbers
+
+public class sum {
+    public static void main(String[] args) {
+        int num=100,sum=0;
+        for (int i=1;i<=num;i++)
+        {
+            sum = sum + i;
+            System.out.println(sum);
+        }
+    }
+}
